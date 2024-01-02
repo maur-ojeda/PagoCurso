120a24a43152a72c9766cad548d4fb881954e08a
maur-ojeda <49486702+maur-ojeda@users.noreply.github.com>
2024-01-02 13:41:08 -0300

    entidad user

---
+++ src/Entity/User.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $surname = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $maternalSurname = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $documentId = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $username = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $phone = null;
+
+    #[ORM\Column]
+    private ?int $schoolYearId = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSurname(): ?string
+    {
+        return $this->surname;
+    }
+
+    public function setSurname(string $surname): static
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    public function getMaternalSurname(): ?string
+    {
+        return $this->maternalSurname;
+    }
+
+    public function setMaternalSurname(?string $maternalSurname): static
+    {
+        $this->maternalSurname = $maternalSurname;
+
+        return $this;
+    }
+
+    public function getDocumentId(): ?int
+    {
+        return $this->documentId;
+    }
+
+    public function setDocumentId(int $documentId): static
+    {
+        $this->documentId = $documentId;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): static
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getSchoolYearId(): ?int
+    {
+        return $this->schoolYearId;
+    }
+
+    public function setSchoolYearId(int $schoolYearId): static
+    {
+        $this->schoolYearId = $schoolYearId;
+
+        return $this;
+    }
+}
